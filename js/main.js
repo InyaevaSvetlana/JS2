010b76a35e16e6e72e1a6e852bb0c78f41df2e9b
InyaevaSvetlana <58864697+InyaevaSvetlana@users.noreply.github.com>
2021-01-13 21:23:44 +0300

    lesson1

--- js/main.js
+++ js/main.js
@@ -5,18 +5,20 @@ const products = [
     { id: 2, title: 'Keyboard', price: 200 },
     { id: 3, title: 'Mouse', price: 100 },
     { id: 4, title: 'Gamepad', price: 87 },
+    
 ];
 
 
-const renderProduct = (title, price) => {
+const renderProduct = (title, price, img = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTSjw-6Mc1f6VYw9hGNwuwthHgbs0mMYiph2w&usqp=CAU" ) => {
     return `<div class="product-item">
-                <h3>${title}</h3>
-                <p>${price}</p>
+    <img src="${img}" alt="image" class="product-img">
+                <h3 class="product-title">${title}</h3>
+                <p class="product-price">${price}</p>
             </div>`
 };
 
 const render = productsList => {
-    const productsElements = productsList.map(item => renderProduct(item.title, item.price));
+    const productsElements = productsList.map(item => renderProduct(item.title, item.price, item.img));
     document.querySelector('.products').innerHTML = productsElements;
 };
 
