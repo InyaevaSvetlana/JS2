37738f88f03bf54143ec12ccfb82c1c6a15b688f
InyaevaSvetlana <58864697+InyaevaSvetlana@users.noreply.github.com>
2021-01-19 11:39:27 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,25 +1,65 @@
 
+class Product {
+    constructor(product, img = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTSjw-6Mc1f6VYw9hGNwuwthHgbs0mMYiph2w&usqp=CAU') {
+        let { title, price = 0, id } = product;
+        this.title = title;
+        this.img = img;
+        this.price = price;
+        this.id = id;
+    }
 
-const products = [
-    { id: 1, title: 'Notebook', price: 2000 },
-    { id: 2, title: 'Keyboard', price: 200 },
-    { id: 3, title: 'Mouse', price: 100 },
-    { id: 4, title: 'Gamepad', price: 87 },
-    
-];
+    render() {
+        return `<div class="product-item">
+                  <img src="${this.img}" alt="${this.title}">
+                  <div class="desc">
+                      <h3>${this.title}</h3>
+                      <p>${this.price}</p>
+                      <button class="buy-btn">Купить</button>
+                  </div>
+              </div>`
+    }
+
+}
+
+class ProductsList {
+    constructor(container = '.products') {
+        this.data = [];
+        this.products = [];
+        this.container = document.querySelector(container);
+        this._fetchData();
+        this._render();
+    }
+
+    init() {}
+
+    _fetchData() {
+        this.data = [
+            { id: 1, title: 'Notebook', price: 2000 },
+            { id: 2, title: 'Keyboard', price: 200 },
+            { id: 3, title: 'Mouse', price: 100 },
+            { id: 4, title: 'Gamepad', price: 150 },
+        ];
+    }
+
+    _render() {
+        for (let dataEl of this.data) {
+            const product = new Product(dataEl);
+            this.products.push(product);
+            this.container.insertAdjacentHTML('beforeend', product.render())
+        }
+    }
+}
+
+const list = new ProductsList();
+
+class Cart {
+    constructor() {
+        // name - свойство в котором лежит имя
+    }
+
+    // some() - метод для подсчета стоимости корзины
+}
 
 
-const renderProduct = (title, price, img = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTSjw-6Mc1f6VYw9hGNwuwthHgbs0mMYiph2w&usqp=CAU" ) => {
-    return `<div class="product-item">
-    <img src="${img}" alt="image" class="product-img">
-                <h3 class="product-title">${title}</h3>
-                <p class="product-price">${price}</p>
-            </div>`
-};
 
-const render = productsList => {
-    const productsElements = productsList.map(item => renderProduct(item.title, item.price, item.img));
-    document.querySelector('.products').innerHTML = productsElements;
-};
 
-render(products);
